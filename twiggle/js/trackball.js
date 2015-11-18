763044589529411963a41364d64b516bb6101414
mseefelder <saa.marcos@gmail.com>
2015-11-18 14:21:16 -0200

    Still building the foundations for the trackball...

--- twiggle/js/trackball.js
+++ twiggle/js/trackball.js
@@ -18,21 +18,53 @@ twgl.Trackball = function ( canvas ) {
 	this.projectionMatrix = m4.perspective(30 * Math.PI / 180, canvas.clientWidth / canvas.clientHeight, 0.5, 10);
 	this.cameraMatrix = m4.lookAt(this.eye, this.target, this.up);
 	this.viewProjectionMatrix = m4.multiply(m4.inverse(this.cameraMatrix), this.projectionMatrix);
+
+	//transformation matrix
+	this.transformMatrix = m4.identity();
+
 	//internal variables
 	var scope = this;
+	var isRotating = false;
+	var updateRotation = false;
+	var initialVector = v3.create(0,0,0);
+	var finalVector = v3.create(0,0,0);
 	
 	//this.functions
 	this.update = function () {
+		if (updateRotation) {
+			//update transform matrix with rotation
+			//...
+			//rotation updated. set as false
+			updateRotation = false;
+		}
 		this.projectionMatrix = m4.perspective(30 * Math.PI / 180, canvas.clientWidth / canvas.clientHeight, 0.5, 10);
 		this.cameraMatrix = m4.lookAt(this.eye, this.target, this.up);
 		this.viewProjectionMatrix = m4.multiply(m4.inverse(this.cameraMatrix), this.projectionMatrix);
 	};
 
+	//takes in two vector components supposed to be between -1.0 and 1.0
+	this.rotate = function ( x, y ) {
+		if (!isRotating) {
+			isRotating = true;
+			initialVector = projectOnSphere(x,y);
+		}
+		else {
+			finalVector = projectOnSphere(x,y);
+			updateRotation = true;
+		}
+	};
 
+	this.endRotation = function ( x, y ) {
+		if (isRotating) {
+			finalVector = projectOnSphere(x,y);
+			updateRotation = true;
+			isRotating = false;
+		}
+	};
 
 	//
 	function onMouseDown( event ){
-
+		
 	}
 
 	function onMouseWheel( event ){
