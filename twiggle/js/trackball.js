fe0427ba71cff65b83a087cc3db8f1b4f6386f07
mseefelder <saa.marcos@gmail.com>
2015-12-01 16:46:38 -0200

    Trackball works perfectly! Just had to stop doing stuff at endRotation().

--- twiggle/js/trackball.js
+++ twiggle/js/trackball.js
@@ -22,7 +22,7 @@ twgl.Trackball = function ( canvas ) {
 	//projection "lenses"
 	this.projectionMatrix = m4.perspective(30 * Math.PI / 180, canvas.clientWidth / canvas.clientHeight, 0.5, 10);
 	this.cameraMatrix = m4.lookAt(this.eye, this.target, this.up);
-	this.viewProjectionMatrix = m4.multiply(this.projectionMatrix, m4.inverse(this.cameraMatrix));//m4.multiply(m4.inverse(this.cameraMatrix), this.projectionMatrix);//
+	this.viewProjectionMatrix = m4.multiply(this.projectionMatrix, m4.inverse(this.cameraMatrix));
 
 	//transformation matrix
 	this.oldTransformMatrix = m4.identity();
@@ -31,7 +31,7 @@ twgl.Trackball = function ( canvas ) {
 	//internal variables
 	var scope = this;
 	var isRotating = false;
-	var updateRotation = false;
+	var newRotation = false;
 	var initialVector = v3.copy([0,0,0]);
 	var finalVector = v3.copy([0,0,0]);
 	
@@ -39,32 +39,26 @@ twgl.Trackball = function ( canvas ) {
 	this.update = function () {
 		this.projectionMatrix = m4.perspective(30 * Math.PI / 180, canvas.clientWidth / canvas.clientHeight, 0.5, 10);
 		this.cameraMatrix = m4.lookAt(this.eye, this.target, this.up);
-		this.viewProjectionMatrix = m4.multiply(this.projectionMatrix, m4.inverse(this.cameraMatrix));//m4.multiply(m4.inverse(this.cameraMatrix), this.projectionMatrix);//
+		this.viewProjectionMatrix = m4.multiply(this.projectionMatrix, m4.inverse(this.cameraMatrix));
 	};
 	this.updateRotation = function () {
-		if (updateRotation) {
+		if (newRotation) {
 			//update transform matrix with rotation
 			var dot = v3.dot(initialVector,finalVector);
 			var angle = (dot <= 1) ? Math.acos(dot) : 0.0;
-			//rotation damping;
-			//angle = 0.8 * angle;
 
 			var axis = v3.cross(initialVector,finalVector);
 			if(v3.lengthSq(axis) != 0) {
 				v3.normalize(axis,axis);
 			}
 
-			//axis = m4.transformDirection(m4.inverse(m4.multiply(this.oldTransformMatrix, m4.inverse(this.cameraMatrix))), axis);
 			axis = m4.transformDirection(m4.inverse(m4.multiply(m4.inverse(this.cameraMatrix), this.oldTransformMatrix)), axis);
 
 			var q = quaternionRotationMatrix( axis, angle );
 			m4.multiply(this.oldTransformMatrix, q, this.transformMatrix);
-			//commented for now
-			//m4.axisRotate(this.oldTransformMatrix, axis, angle, this.transformMatrix);
-
 			
 			//rotation updated. set as false
-			updateRotation = false;
+			newRotation = false;
 		}
 	};
 
@@ -79,7 +73,7 @@ twgl.Trackball = function ( canvas ) {
 		}
 		else {
 			finalVector = projectOnSphere(x,y,1.0);
-			updateRotation = true;
+			newRotation = true;
 		}
 		/**/
 	};
@@ -87,8 +81,7 @@ twgl.Trackball = function ( canvas ) {
 	this.endRotation = function ( x, y ) {
 		console.log("end of rotation");
 		if (isRotating) {
-			finalVector = projectOnSphere(x,y);
-			updateRotation = true;
+			newRotation = false;
 			isRotating = false;
 		}
 	};
@@ -138,15 +131,6 @@ twgl.Trackball = function ( canvas ) {
 
 		return rot;
 	}
-
-	function onMouseDown( event ){
-		
-		
-	}
-
-	function onMouseWheel( event ){
-
-	}
 	
 	/*
 	this.canvas.addEventListener( 'canvasmenu', function ( event ) { event.preventDefault(); }, false );
@@ -155,97 +139,3 @@ twgl.Trackball = function ( canvas ) {
 	this.canvas.addEventListener( 'DOMMouseScroll', onMouseWheel, false ); // firefox
 	*/
 };
-
-//twgl.Trackball.prototype = Object.create( twgl.EventDispatcher.prototype );
-
-/*
-	twgl.EventDispatcher = function () {}
-
-	twgl.EventDispatcher.prototype = {
-
-		constructor: twgl.EventDispatcher,
-
-		apply: function ( object ) {
-
-			object.addEventListener = twgl.EventDispatcher.prototype.addEventListener;
-			object.hasEventListener = twgl.EventDispatcher.prototype.hasEventListener;
-			object.removeEventListener = twgl.EventDispatcher.prototype.removeEventListener;
-			object.dispatchEvent = twgl.EventDispatcher.prototype.dispatchEvent;
-
-		},
-
-		addEventListener: function ( type, listener ) {
-
-			if ( this._listeners === undefined ) this._listeners = {};
-
-			var listeners = this._listeners;
-
-			if ( listeners[ type ] === undefined ) {
-
-				listeners[ type ] = [];
-
-			}
-
-			if ( listeners[ type ].indexOf( listener ) === - 1 ) {
-
-				listeners[ type ].push( listener );
-
-			}
-
-		},
-
-		hasEventListener: function ( type, listener ) {
-
-			if ( this._listeners === undefined ) return false;
-
-			var listeners = this._listeners;
-
-			if ( listeners[ type ] !== undefined && listeners[ type ].indexOf( listener ) !== - 1 ) {
-
-				return true;
-
-			}
-
-			return false;
-
-		},
-
-		removeEventListener: function ( type, listener ) {
-
-			if ( this._listeners === undefined ) return;
-
-			var listeners = this._listeners;
-			var index = listeners[ type ].indexOf( listener );
-
-			if ( index !== - 1 ) {
-
-				listeners[ type ].splice( index, 1 );
-
-			}
-
-		},
-
-		dispatchEvent: function ( event ) {
-
-			if ( this._listeners === undefined ) return;
-
-			var listeners = this._listeners;
-			var listenerArray = listeners[ event.type ];
-
-			if ( listenerArray !== undefined ) {
-
-				event.target = this;
-
-				for ( var i = 0, l = listenerArray.length; i < l; i ++ ) {
-
-					listenerArray[ i ].call( this, event );
-
-				}
-
-			}
-
-		}
-
-	};
-*/
-
